6f434b0dfef66ce2dbbb057c455a400ae034b958
Max Deutsch <max.deutsch@postoe.at>
2022-12-07 15:31:46 +0100

    Add target fps input

--- TextureRectFinalDisplay.gd
+++ TextureRectFinalDisplay.gd
@@ -4,12 +4,16 @@ onready var Sprite1 = $"/root/RootControl/Viewport1/Sprite1"
 onready var Sprite2 = $"/root/RootControl/Viewport2/Sprite2"
 onready var TreeProbInput = $"/root/RootControl/UIContainer/HBoxContainer/TreeProbInput"
 onready var FireProbInput = $"/root/RootControl/UIContainer/HBoxContainer/FireProbInput"
+onready var TargetFPSInput = $"/root/RootControl/UIContainer/HBoxContainer/TargetFPSInput"
 
 func _ready():
 	TreeProbInput.connect("value_changed", self, "on_tree_prob_change")
 	FireProbInput.connect("value_changed", self, "on_fire_prob_change")
+	TargetFPSInput.connect("value_changed", self, "on_target_fps_change")
 	
-	Engine.set_target_fps(0)
+	TreeProbInput.set_value(0.5);
+	FireProbInput.set_value(0.0);
+	TargetFPSInput.set_value(0.0);
 
 func on_tree_prob_change(value):
 	Sprite1.material.set_shader_param("treeProbability", value / 100.0)
@@ -19,6 +23,8 @@ func on_fire_prob_change(value):
 	Sprite1.material.set_shader_param("fireProbability", value / 100.0)
 	Sprite2.material.set_shader_param("fireProbability", value / 100.0)
 	
+func on_target_fps_change(value):
+	Engine.set_target_fps(value)
 
 func _input(event: InputEvent):
 	if not event is InputEventMouse:
