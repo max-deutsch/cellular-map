19ec740232dc014585063a64f93f7aa48f71dd8f
Max Deutsch <max.deutsch@postoe.at>
2022-12-07 15:24:36 +0100

    unlock fps

--- TextureRectFinalDisplay.gd
+++ TextureRectFinalDisplay.gd
@@ -3,12 +3,21 @@ extends TextureRect
 onready var Sprite1 = $"/root/RootControl/Viewport1/Sprite1"
 onready var Sprite2 = $"/root/RootControl/Viewport2/Sprite2"
 onready var TreeProbInput = $"/root/RootControl/UIContainer/HBoxContainer/TreeProbInput"
+onready var FireProbInput = $"/root/RootControl/UIContainer/HBoxContainer/FireProbInput"
 
 func _ready():
 	TreeProbInput.connect("value_changed", self, "on_tree_prob_change")
+	FireProbInput.connect("value_changed", self, "on_fire_prob_change")
+	
+	Engine.set_target_fps(0)
 
 func on_tree_prob_change(value):
-	Sprite1.material.set_shader_param("treeProbability", value/100.0)
+	Sprite1.material.set_shader_param("treeProbability", value / 100.0)
+	Sprite2.material.set_shader_param("treeProbability", value / 100.0)
+	
+func on_fire_prob_change(value):
+	Sprite1.material.set_shader_param("fireProbability", value / 100.0)
+	Sprite2.material.set_shader_param("fireProbability", value / 100.0)
 	
 
 func _input(event: InputEvent):
